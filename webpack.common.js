f24ed0f7d793848b4dab1b6ebb09eaefa2bcebed
Felipe Nava <felipe.nava.co@gmail.com>
2020-03-15 13:11:23 -0700

    feat: adding config for webpack for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/app.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.min.js',
+  },
+};
